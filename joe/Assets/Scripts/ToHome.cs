1b631f979f36d3a033a79492864b633d11ef86e1
dazai <mydataisnotforfbi@proton.me>
2023-02-28 21:47:55 +0100

    21.47

---
+++ joe/Assets/Scripts/ToHome.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ToHome : MonoBehaviour
+{
+    public bool pause = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("Home");
+        }
+
+    }
+    public void Exit(){
+        Application.Quit();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && pause == false){
+            Time.timeScale = 0f;
+            //pause = true;
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) && pause == true){
+            Time.timeScale = 1f;
+            pause = false;
+        }
+    }
+    public void Resume(){
+        Time.timeScale = 1f;
+    }
+}
