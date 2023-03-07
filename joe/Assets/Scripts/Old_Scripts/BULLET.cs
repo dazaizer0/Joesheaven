a4b555dafc0479bba517c67bfa23724d56f6f365
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-03-07 23:17:27 +0100

    Update BULLET.cs

--- joe/Assets/Scripts/Old_Scripts/BULLET.cs
+++ joe/Assets/Scripts/Old_Scripts/BULLET.cs
@@ -20,33 +20,39 @@ public class BULLET : MonoBehaviour
 
     IEnumerator DeathDelay()
     {
-        
 
         yield return new WaitForSeconds(lifeTime);
         Destroy(gameObject);
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.tag == "stop")
         {
+
             playEffect();
 
             GetComponent<Collider2D>();
             Destroy(gameObject);
-
         }
+
         if (other.tag == "petfrog")
         {
-            playEffect();
 
+            playEffect();
         }
+
         if (other.tag == "damage")
         {
+
             playEffect();
         }
     }
+
     public void playEffect()
     {
+
         Instantiate(effect, transform.position, Quaternion.identity);
     }
 }
