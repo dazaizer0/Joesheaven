6f5a555e0cdd5829a71aac62aeb6342e4c363560
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-03-07 23:13:07 +0100

    Update fallowingEnemy.cs

--- joe/Assets/Scripts/Enemy's/fallowingEnemy.cs
+++ joe/Assets/Scripts/Enemy's/fallowingEnemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class fallowingEnemy : MonoBehaviour
 {
+
     public float health;
     public BoxCollider2D bc2d;
 
@@ -18,6 +19,7 @@ public class fallowingEnemy : MonoBehaviour
 
     void Start()
     {
+
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
@@ -26,6 +28,7 @@ public class fallowingEnemy : MonoBehaviour
         
         if (health <= 0)
         {
+
             Vector3 effectPos = new Vector3(transform.position.y, transform.position.y);
             Instantiate(effect, effectPos, Quaternion.identity);
 
@@ -34,34 +37,41 @@ public class fallowingEnemy : MonoBehaviour
 
         if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
         {
+
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }
-        else if ((Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance))
+        }else if ((Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance))
         {
+
             transform.position = this.transform.position;
-        }
-        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        }else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
         {
+
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
+
         if (other.tag == "bullet")
         {
+
             GetComponent<BoxCollider2D>();
             damage();
 
         }
+
         if (other.tag == "petfrog")
         {
+
             GetComponent<BoxCollider2D>();
             damage();
-
         }
     }
+
     public void damage()
     {
+
         GetComponent<BoxCollider2D>();
         Debug.Log("dmage have been taken");
         health -= geting_damage;
