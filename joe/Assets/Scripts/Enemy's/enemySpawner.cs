ea98c4816aab6b46d9465cf3747f1c70e3d67294
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-03-07 23:12:23 +0100

    Update enemySpawner.cs

--- joe/Assets/Scripts/Enemy's/enemySpawner.cs
+++ joe/Assets/Scripts/Enemy's/enemySpawner.cs
@@ -27,56 +27,68 @@ public class enemySpawner : MonoBehaviour
 
     void Start()
     {
+
         //StartCoroutine(spawnEnemy(enemyAInterval, enemy_a));
         //StartCoroutine(spawnEnemy(enemyFInterval, enemy_f));
     }
+
     void Update()
     {
+
         Destroy(gameObject, lt);
+
         if(start == false)
         {
+
             StartCoroutine(spawnEnemy(enemyAInterval, enemy_a));
             StartCoroutine(spawnEnemy(enemyFInterval, enemy_f));
             start = true;
 
         }else
         {
+
             Debug.Log("started");
         }
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
+
         yield return new WaitForSeconds(interval);
 
         int sp = Random.Range(1, 5);
 
         if(sp == 1)
         {
+
             Vector3 pos = new Vector3(sp1.transform.position.x, sp1.transform.position.y);
             GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
         }
 
         if(sp == 2)
         {
+
             Vector3 pos = new Vector3(sp2.transform.position.x, sp2.transform.position.y);
             GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
         }
 
         if(sp == 3)
         {
+
             Vector3 pos = new Vector3(sp3.transform.position.x, sp3.transform.position.y);
             GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
         }
 
         if(sp == 4)
         {
+
             Vector3 pos = new Vector3(sp4.transform.position.x, sp4.transform.position.y);
             GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
         }
 
         if(sp == 5)
         {
+
             Vector3 pos = new Vector3(sp5.transform.position.x, sp5.transform.position.y);
             GameObject newEnemy = Instantiate(enemy, pos, Quaternion.identity);
         }
